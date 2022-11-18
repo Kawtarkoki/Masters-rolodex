ea11c5c286eb5ba32ab9f817fd08c9e6a23b82d0
bakour <akawtarbakour@gmail.com>
2022-11-18 14:21:35 +0100

    Storing Original data

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     super()
     this.state = { 
       monsters : [],
+      searchField: '',
     };
-      
   }
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -23,30 +23,28 @@ class App extends Component {
       ));
   }
   render() { 
+    
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    })
+
     return (
       <div className="App">
-
         <input
         className='search-box'
         type='search'
         placeholder='Tap to search for monsters'
         onChange={ (event) => {
-          console.log(event.target.value)
 
-          const searchString = event.target.value.toLocaleLowerCase()
-          const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchString);
-          })
-
+          const searchField = event.target.value.toLocaleLowerCase()
           this.setState(() => {
-            return { monsters: filteredMonsters };
+            return { searchField };
           })
 
         }}
         />
-
         {
-          this.state.monsters.map ( (monster) => {
+          filteredMonsters.map ( (monster) => {
             return <div key={monster.id}>
               <h4>{monster.name}</h4>
               </div>               
