381adc0ae375210e044bcafac599279bee70f2fe
bakour <akawtarbakour@gmail.com>
2022-11-21 11:13:31 +0100

    Optimizations : convert class component over into a functional component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,14 +1,13 @@
-import { Component } from 'react';
+// functional component 
+
 import './card.styles.css';
 
-class Card extends Component {
-    render() { 
-        const {id, name, email} = this.props.monster;
+const Card = ({monster})  => {
+
+        const {id, name, email} = monster;
         
         return (
-
             <div className='card-container' key={id}>
-
             <img
                 alt={`monster ${name}`}
                 src={`https://robohash.org/${id}/?set=set2&size=180x180`}
@@ -17,7 +16,32 @@ class Card extends Component {
             <p>{email}</p>
             </div>
         );
-    }
 }
- 
+
 export default Card;
+
+
+// Class component 
+// import { Component } from 'react';
+// import './card.styles.css';
+
+// class Card extends Component {
+//     render() { 
+//         const {id, name, email} = this.props.monster;
+        
+//         return (
+
+//             <div className='card-container' key={id}>
+
+//             <img
+//                 alt={`monster ${name}`}
+//                 src={`https://robohash.org/${id}/?set=set2&size=180x180`}
+//             />
+//             <h2>{name}</h2>
+//             <p>{email}</p>
+//             </div>
+//         );
+//     }
+// }
+ 
+// export default Card;
